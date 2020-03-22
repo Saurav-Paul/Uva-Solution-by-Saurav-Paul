ca7cdc8a986429f67c297ba4e9354b1934f46a06
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2020-03-22 13:43:11 +0600

    Create 113 - Power of Cryptography .cpp

---
+++ 113 - Power of Cryptography .cpp
@@ -0,0 +1,13 @@
+				/*Saurav Paul*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+   double n , p ;
+   while( cin >> n >> p){
+       printf("%.0lf\n",pow(p , 1/n));
+   } 
+
+    return 0;
+}
